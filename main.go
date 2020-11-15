1db52c3180f59b3558d2fbb1b2b4261eb2311c03
Pluto Chan <pumsscco@163.com>
2020-11-15 22:37:51 +0800

    规划配置文件与日志，要参考gin框架本身的解决方案，不直接用标准库的，否则用框架不能充分发挥，就没意义了

--- main.go
+++ main.go
@@ -2,8 +2,64 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"fmt"
+    "database/sql"
+    "log"
+    yaml "gopkg.in/yaml.v2"
+	"github.com/go-redis/redis"
+	"io/ioutil"
+    _ "github.com/go-sql-driver/mysql"
+    "os"
 )
-
+type Conf struct {
+    MySQL struct {
+        Db string `yaml:"db"`
+        Host string `yaml:"host"`
+        Port int `yaml:"port"`
+        User string `yaml:"user"`
+        Pass string `yaml:"pass"`
+    }
+    Redis struct {
+        Host string `yaml:"host"`
+        Port int `yaml:"port"`
+        Pass string `yaml:"pass"`
+    }
+}
+var (
+    Db *sql.DB
+    logger *log.Logger
+    client *redis.Client
+    cnf Conf
+)
+//所有初始化操作
+func init() {
+    //抓全部的配置信息
+    yamlBytes, err := ioutil.ReadFile("config.yml")
+    if err!=nil {
+        log.Fatalf("无法打开环境配置文件: %v",err)
+    }
+    yaml.Unmarshal(yamlBytes,&cnf)
+    file, err := os.OpenFile("gin.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("无法打开日志文件：%v\n", err)
+    }
+    logger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+    client=redis.NewClient(&redis.Options{
+        Addr:       fmt.Sprintf("%s:%d",cnf.Redis.Host,cnf.Redis.Port),
+        Password:   cnf.Redis.Pass,
+        DB:         0,
+    })
+    _, err = client.Ping().Result()
+    if err!=nil {
+        logger.Fatalf("redis连接异常：%v\n",err)
+    }    
+    dsn:=fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?loc=Local&parseTime=true", cnf.MySQL.User, cnf.MySQL.Pass, cnf.MySQL.Host, cnf.MySQL.Port, cnf.MySQL.Db)
+    //fmt.Println("Data Source Name: ",dsn)
+    Db,err=sql.Open("mysql",dsn)
+    if err!=nil {
+        logger.Fatalf("mysql连接异常：%v\n",err)
+    }
+}
 func main() {
 	router := gin.Default()
 	//以项目为分组，内部接口全面压缩整合
@@ -11,7 +67,7 @@ func main() {
 	attendance := router.Group("/attendance")
 	{
 		//依据所带参数是latest/last-week/last-month，转向不同的处理函数或干脆合并，分情形输出，减少接口
-		attendance.POST("/rec",rec) 
+		attendance.POST("/rec",rec)
 		//与上面采取类似的思路
 		attendance.POST("/stat",stat)
 		attendance.POST("/add", add)
